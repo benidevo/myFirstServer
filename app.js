0e066c6672b141e794d3b0e46e00a33df6737628
Benjamin Idewor <benjaminidewor@gmail.com>
2021-11-22 21:44:20 +0100

    feat: display html page

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require("path");
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.use(express.static("public"))
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
 });
-
